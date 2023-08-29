db56c988304a86252b5f89d73196ce2c313a847b
sumitwaghmare4500 <sumitwaghmare8083@gmail.com>
2023-08-29 12:43:44 +0530

    use the usecontext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect ,useReducer} from 'react';
+import React, { useState, useEffect ,useReducer,useContext} from 'react';
 
 import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
+
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
+import Input from '../UI/input/input';
+import classes from './Login.module.css';
 const emailReducer=(state,action)=>{
   if(action.type==='USER_INPUT')
   return{ value:action.val,isValid:action.val.includes('@')}
@@ -48,7 +51,7 @@ const[emailState,dispatchEmail]=useReducer(emailReducer,{value:'',isValid:false}
       event.target.value.includes('@') && enteredPassword.trim().length > 6
     );
   };
-
+const authCtx=useContext(AuthContext)
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
@@ -67,40 +70,28 @@ const[emailState,dispatchEmail]=useReducer(emailReducer,{value:'',isValid:false}
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    authCtx.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input  id="email" 
+        label="E-Mail"
+        type="email"
+        isValid={emailState.isValid}
+        value={emailState.value}
+        onChange={emailChangeHandler}
+        onBlur={validateEmailHandler}/>
+
+        <Input  id="password" 
+        label="Passwoed"
+        type="password"
+        isValid={passwordState.isValid}   
+        value={passwordState.value}  
+        onChange={passwordChangeHandler}
+        onBlur={validatePasswordHandler}/>
+       
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState ,useEffect} from "react";
 const AuthContext=React.createContext({
-    isLoggedIn:false
+    isLoggedIn:false,
+    onLogout:()=>{},
+    onLogin:(email,password)=>{}
 })
+export const  AuthContextProvider=(props)=>{
+    const [isLoggedIn,setIsloggedIn]=useState(false)
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    
+        if (storedUserLoggedInInformation === '1') {
+            setIsloggedIn(true);
+        }
+      }, []);
+    const logoutHandler=()=>{
+        localStorage.removeItem('isLoggedIn');
+        setIsloggedIn(false)
+    }
+    const loginHandler=()=>{
+        localStorage.setItem('isLoggedIn', '1');
+        setIsloggedIn(true)
+    }
+    return(
+        <AuthContext.Provider
+        value={{isLoggedIn:isLoggedIn,
+        onLogout:logoutHandler,
+    onLogin:loginHandler}}
+        >{props.children}</AuthContext.Provider>
+    )
+}
 export default AuthContext
